f453f17f1ec178677594f4cc27ea8ee96fc400d0
王希宁 <xiningwang120@gmail.com>
2018-01-26 20:58:17 -0600

    add basic two-page routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+
+const appRoutes: Routes = [
+    { path: 'crisis-center', component: CrisisListComponent},
+    { path: 'heroes', component: HeroListComponent },
+    { path: '', redirectTo: '/heroes', pathMatch: 'full'},
+    { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
